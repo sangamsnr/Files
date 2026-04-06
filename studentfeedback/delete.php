3c9b87fb1e8f82032a69c599b3dbe5e895d8417b
sangamsnr <sangamsunar444@gmail.com>
2026-04-06 14:08:28 +0545

    Delete studentfeedback directory

--- studentfeedback/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("configASL.php");
-$id=$_GET['del'];
-mysqli_query($al,"delete from faculty where id='$id'");
-?>
-<script type="text/javascript">
-alert("Successfully deleted");
-window.location='manageFaculty.php';
-</script>
